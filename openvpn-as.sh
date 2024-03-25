b316d7cad4be141e6b792fc8439aa8db6c62077c
nu11secur1ty <venvaropt@gmail.com>
2024-03-25 16:37:31 +0200

    Update openvpn-as.sh

--- openvpn-as.sh
+++ openvpn-as.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/bash
 # nu11secur1ty - OpenVPN - GUI
  
-apt update && apt -y install ca-certificates wget net-tools gnupg
+apt update && apt upgrade && apt dist-upgrade -y 
+apt install ca-certificates wget net-tools gnupg -y
 wget https://as-repository.openvpn.net/as-repo-public.asc -qO /etc/apt/trusted.gpg.d/as-repository.asc
 echo "deb [arch=amd64 signed-by=/etc/apt/trusted.gpg.d/as-repository.asc] http://as-repository.openvpn.net/as/debian jammy main">/etc/apt/sources.list.d/openvpn-as-repo.list
-apt update && apt -y install openvpn-as
+apt update && apt install openvpn-as -y
