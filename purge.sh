a41c4312049fac46514515de35cbdf346e8b6375
nu11secur1ty <venvaropt@gmail.com>
2024-03-27 11:41:36 +0200

    Update purge.sh

--- purge.sh
+++ purge.sh
@@ -4,6 +4,7 @@
 apt purge openvpn-as -y
 rm -rf /usr/local/openvpn_as/
 rm -rf /etc/apt/sources.list.d/openvpn-as-repo.list
+rm -rf /etc/apt/trusted.gpg.d/as-repository.asc
 apt update -y
 apt upgrade -y
 apt autoremove -y
